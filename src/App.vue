ebf3284d0ffcba1d26429675e642eb6a859d6df1
Harry Barber <harrybarber@protonmail.com>
2019-12-18 18:50:52 +0700

    Minimal failure example

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+var grpc = require('grpc')
+
+export default {
+  created () {
+    grpc.getClientChannel() // This can be anything involving gRPC
+  }
+}
+</script>
